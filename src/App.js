01f03411e92a528e2b2ba5043b9ec591899e83b8
Arun Jacob <arun.jacob@kointrack.com>
2022-05-17 11:03:46 +0530

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import react, { useState } from 'react';
 function App() {
     const [first, setfirst] = useState(second);
 
-    return <div className="App">dsjflskfdj</div>;
+    return (
+        <div className="App">
+            {' '}
+            <div className="yoyo"></div>
+        </div>
+    );
 }
 
 export default App;
