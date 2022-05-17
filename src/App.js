f65df4f6264704c625eef53f691069f972a60606
Arun Jacob <arun.jacob@kointrack.com>
2022-05-17 11:04:14 +0530

    test

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
 
     return (
         <div className="App">
-            {' '}
-            <div className="yoyo"></div>
+            <div className="yoyo">lsnfdknflssdf</div>
         </div>
     );
 }
